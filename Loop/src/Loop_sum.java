a5f89eac055160b759c99b6ce3d8c0e563fe8cd7
Jiyoo lee <103879030+jiyoo-lee@users.noreply.github.com>
2022-05-25 21:44:01 +0900

    Add files via upload

---
+++ Loop/src/Loop_sum.java
@@ -0,0 +1,15 @@
+
+public class Loop_sum {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int gugu = 2;
+
+		for (int i = 1; i <= 9; i++) {
+			sum += i * gugu;
+		}
+		System.out.println(sum);
+
+	} // end of main
+
+}
