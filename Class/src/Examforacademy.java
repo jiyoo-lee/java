46260dbface23e88078f05873771f88ef3cdf6ac
Jiyoo lee <103879030+jiyoo-lee@users.noreply.github.com>
2022-06-02 21:19:46 +0900

    Update Examforacademy.java

--- Class/src/Examforacademy.java
+++ Class/src/Examforacademy.java
@@ -2,15 +2,23 @@
 public class Examforacademy {
 
 	public static void main(String[] args) {
-		//1. Editor�� ���� ?  
-		//2. Database�� ����?
-		//3. Java���� ����ϴ� �ڷ����� �ƴ� ����?
-		//4. Java���� ����ϴ� ���о� �߸��Ȱ���?
-		//ex) project��? package��? import��? class��?
-		//5. java�� ���� ���� �� �߸��� ����? (���� java�� ���� x, java�� Ư¡��(������,ĸ��ȭ,���ȭ,�߻�ȭ)
-		//6. ���� �����ڵ� �� �߸��� �κ���? 
-		// ������ �ݿ��Ͽ� ����~ �Ǳ�(���ǹ�,�ݺ���) 70 / �ʱ� 30��
+		//1. Editor의 종류 ? eclipse, j builder editplus intellij
+		//2. Database의 종류? 
+		//관계형 데이터 베이스(RDB): 흔히 행과 열로 표현되는 테이블간의 관계를 나타낼때 사용하며 종류로는 Oracle, MySQL, MS-SQL,DB2,Infomix(IBM)
+		//,Maria DB(Sun Microsystems),Derby(Apache),SQLite(Opensource),중복을 허용하지않는다.
+		//NoSQL(Not Only SQL): RDB의 특성뿐만 아니라 다른 특성까지도 지원해주는 데이터베이스라는 의미를 가진다. 중복을 허용한다. 
+		// 장점: 대용량 데이터를 다루거나 데이터 분산 처리에 용이. 유연한 데이터 모델링이 가능. Cloud computing에 적합.
+		// 종류: document방식
+		// key-value방식
+		// big table db
+		//3. Java에서 사용하는 자료형이 아닌 것은?
+		//4. Java에서 사용하는 구분언어가 잘못된것은?
+		//ex) project란? package란? import란? class란?
+		//5. java에 대한 설명 중 잘못된 것은? (절대 java의 역사 x, java의 특징ㅇ(다형성,캡슈화,상속화,추상화)
+		//6. 다음 문법코드 중 잘못된 부분은? 
+		// 다음주 금요일에 시험~ 실기(조건문,반복문) 70 / 필기 30점
 
 	}
 
 }
+
